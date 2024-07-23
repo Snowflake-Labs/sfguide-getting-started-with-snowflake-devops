3bbf0429dadcf3d2e09fe2e9b19fc10e9d6d35e1
jahnvees <you@example.com>
2024-07-23 18:25:17 +0530

    Testing use case

--- steps/03_harmonize_data.sql
+++ steps/03_harmonize_data.sql
@@ -112,3 +112,34 @@ create or replace view weather_joined_with_major_cities as
   join zip_codes_in_city zip on city.geo_id = zip.city_geo_id
   join weather_forecast weather on zip.zip_geo_name = weather.postal_code
   group by city.geo_id, city.geo_name, city.total_population;
+
+create or replace view attractions as select
+    city.geo_id,
+    city.geo_name,
+    count(case when category_main = 'Aquarium' THEN 1 END) aquarium_cnt,
+    count(case when category_main = 'Zoo' THEN 1 END) zoo_cnt,
+    count(case when category_main = 'Korean Restaurant' THEN 1 END) korean_restaurant_cnt,
+from us_points_of_interest__addresses.cybersyn.point_of_interest_index poi
+join us_points_of_interest__addresses.cybersyn.point_of_interest_addresses_relationships poi_add on poi_add.poi_id = poi.poi_id
+join us_points_of_interest__addresses.cybersyn.us_addresses address on address.address_id = poi_add.address_id
+join major_us_cities city on city.geo_id = address.id_city
+where true
+    and category_main in ('Aquarium', 'Zoo', 'Korean Restaurant')
+    and id_country = 'country/USA'
+group by city.geo_id, city.geo_name;
+
+create or replace view attractions as select
+    city.geo_id,
+    city.geo_name,
+    count(case when category_main = 'Aquarium' THEN 1 END) aquarium_cnt,
+    count(case when category_main = 'Zoo' THEN 1 END) zoo_cnt,
+    count(case when category_main = 'Korean Restaurant' THEN 1 END) korean_restaurant_cnt,
+from us_points_of_interest__addresses.cybersyn.point_of_interest_index poi
+join us_points_of_interest__addresses.cybersyn.point_of_interest_addresses_relationships poi_add on poi_add.poi_id = poi.poi_id
+join us_points_of_interest__addresses.cybersyn.us_addresses address on address.address_id = poi_add.address_id
+join major_us_cities city on city.geo_id = address.id_city
+where true
+    and category_main in ('Aquarium', 'Zoo', 'Korean Restaurant')
+    and id_country = 'country/USA'
+group by city.geo_id, city.geo_name;
+
--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -12,6 +12,9 @@ create or alter table vacation_spots (
   , avg_relative_humidity_pct float
   , avg_cloud_cover_pct float
   , precipitation_probability_pct float
+  , aquarium_cnt int
+, zoo_cnt int
+, korean_restaurant_cnt int
   -- STEP 5: INSERT CHANGES HERE
 ) data_retention_time_in_days = 1;
 
@@ -25,7 +28,7 @@ create or alter task vacation_spots_update
     from silver.flights_from_home flight
     join silver.weather_joined_with_major_cities city on city.geo_name = flight.arrival_city
     join silver.attractions att on att.geo_name = city.geo_name
-    -- STEP 5: INSERT CHANGES HERE
+   join silver.attractions att on att.geo_name = city.geo_name
   ) as harmonized_vacation_spots ON vacation_spots.city = harmonized_vacation_spots.arrival_city and vacation_spots.airport = harmonized_vacation_spots.arrival_airport
   WHEN MATCHED THEN
     UPDATE SET
@@ -38,7 +41,9 @@ create or alter task vacation_spots_update
       , vacation_spots.aquarium_cnt = harmonized_vacation_spots.aquarium_cnt
 ,vacation_spots.zoo_cnt = harmonized_vacation_spots.zoo_cnt
 ,vacation_spots.korean_restaurant_cnt = harmonized_vacation_spots.korean_restaurant_cnt
-      -- STEP 5: INSERT CHANGES HERE
+  , vacation_spots.aquarium_cnt = harmonized_vacation_spots.aquarium_cnt
+, vacation_spots.zoo_cnt = harmonized_vacation_spots.zoo_cnt
+, vacation_spots.korean_restaurant_cnt = harmonized_vacation_spots.korean_restaurant_cnt
   WHEN NOT MATCHED THEN 
     INSERT VALUES (
         harmonized_vacation_spots.arrival_city
@@ -49,7 +54,9 @@ create or alter task vacation_spots_update
       , harmonized_vacation_spots.avg_relative_humidity_pct
       , harmonized_vacation_spots.avg_cloud_cover_pct
       , harmonized_vacation_spots.precipitation_probability_pct
-      -- STEP 5: INSERT CHANGES HERE
+     , harmonized_vacation_spots.aquarium_cnt
+, harmonized_vacation_spots.zoo_cnt
+, harmonized_vacation_spots.korean_restaurant_cnt
     );
 
 
@@ -66,16 +73,16 @@ create or alter task email_notification
         where true
           and punctual_pct >= 50
           and avg_temperature_air_f >= 70
-          and korean_restaurant_cnt > 0
+       and korean_restaurant_cnt > 0
 and (zoo_cnt > 0 or aquarium_cnt > 0)
-          -- STEP 5: INSERT CHANGES HERE
+
         limit 10);
 
 
       if (:options = '[]') then
         CALL SYSTEM$SEND_EMAIL(
             'email_integration',
-            '<insert your email here>', -- INSERT YOUR EMAIL HERE
+            'jahnvee.singh@harman.com, -- INSERT YOUR EMAIL HERE
             'New data successfully processed: No suitable vacation spots found.',
             'The query did not return any results. Consider adjusting your filters.');
       end if;
@@ -88,14 +95,14 @@ and (zoo_cnt > 0 or aquarium_cnt > 0)
 
       CALL SYSTEM$SEND_EMAIL(
         'email_integration',
-        '<insert your email here>', -- INSERT YOUR EMAIL HERE
+        'jahnvee.singh@harman.com, -- INSERT YOUR EMAIL HERE
         'New data successfully processed: The perfect place for your summer vacation has been found.',
         :response);
     exception
         when EXPRESSION_ERROR then
             CALL SYSTEM$SEND_EMAIL(
             'email_integration',
-            '<insert your email here>', -- INSERT YOUR EMAIL HERE
+            'jahnvee.singh@harman.com, -- INSERT YOUR EMAIL HERE
             'New data successfully processed: Cortex LLM function inaccessible.',
             'It appears that the Cortex LLM functions are not available in your region');
     end;
