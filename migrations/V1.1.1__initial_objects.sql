5ee54b3b2262effb44d841826dbfcb0441834658
CHRISTOPHE DESCAMPS <christophe.descamps@finning.com>
2024-09-24 07:00:27 -0700

    migrations

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,6 +1,7 @@
+
 use database demo_db;
-CREATE SCHEMA DEMO;
-CREATE TABLE HELLO_WORLD
+CREATE or replace schema DEMO;
+CREATE or replace table HELLO_WORLD
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
