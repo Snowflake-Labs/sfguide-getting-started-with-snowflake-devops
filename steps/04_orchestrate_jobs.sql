632838bcfe82ab2e6618796657ee920514869987
Hussainparbtani <43732117+Hussainparbtani@users.noreply.github.com>
2025-02-13 21:00:56 +0000

    fixing insert error

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -58,7 +58,7 @@ create or alter task vacation_spots_update
       , harmonized_vacation_spots.aquarium_cnt
       , harmonized_vacation_spots.zoo_cnt
       , harmonized_vacation_spots.korean_restaurant_cnt
-      ,vacation_spots.new_column = {{environment}}
+      , {{environment}}
     );
 
 
