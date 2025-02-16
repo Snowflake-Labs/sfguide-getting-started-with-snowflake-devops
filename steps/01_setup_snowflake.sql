05ac2491bd2a04f7e157503d96de10f6c52803fc
Ashish <ash.kumar@snowflake.com>
2025-02-16 23:29:04 +0530

    parametrize pipeline

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -13,17 +13,17 @@ CREATE OR ALTER DATABASE QUICKSTART_COMMON;
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-ashiskumar') -- INSERT YOUR GITHUB USERNAME HERE
+  API_ALLOWED_PREFIXES = ('https://github.com/Ashish893') -- INSERT YOUR GITHUB USERNAME HERE
   ENABLED = TRUE;
 
 
 -- Git repository object is similar to external stage
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = 'https://github.com/sfc-gh-ashiskumar/sfguide-getting-started-with-snowflake-devops.git'; -- INSERT URL OF FORKED REPO HERE
+  ORIGIN = 'https://github.com/Ashish893/sfguide-getting-started-with-snowflake-devops.git'; -- INSERT URL OF FORKED REPO HERE
 
 
-CREATE OR ALTER DATABASE QUICKSTART_PROD;
+CREATE OR ALTER DATABASE QUICKSTART_{{environment}}; 
 
 
 -- To monitor data pipeline's completion
--- steps/03_harmonize_data.py
+++ steps/03_harmonize_data.py
@@ -267,7 +267,7 @@ pipeline = [
 root = Root(Session.builder.config("connection_name", "snowflake_key_pair").getOrCreate())
 
 # create views in Snowflake
-silver_schema = root.databases["quickstart_prod"].schemas["silver"]
+silver_schema = root.databases[f"quickstart_{os.environ['environment']}"].schemas["silver"]
 silver_schema.user_defined_functions.create(
     map_city_to_airport, mode=CreateMode.or_replace
 )
