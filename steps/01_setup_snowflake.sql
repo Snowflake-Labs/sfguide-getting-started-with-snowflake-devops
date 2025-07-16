ff4264f65e7ebc501927744c7d6bc9b6457c7fba
Thimothe Konchou <mbothe7@hotmail.com>
2025-07-16 17:26:46 +0000

    fixing repos

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -20,7 +20,7 @@ CREATE OR REPLACE API INTEGRATION git_api_integration
 -- Git repository object is similar to external stage
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = 'https://github.com/tmbothe/sfguide-getting-started-with-snowflake-devops'; -- INSERT URL OF FORKED REPO HERE
+  ORIGIN = 'https://github.com/tmbothe/sfguide-getting-started-with-snowflake-devops.git'; -- INSERT URL OF FORKED REPO HERE
 
 
 CREATE OR ALTER DATABASE QUICKSTART_PROD;
